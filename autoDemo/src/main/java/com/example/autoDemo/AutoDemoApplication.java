b0cd891c381bc667fd077c2b2b193647a734e422
luke <https://gitlab.com/luke.lin2com>
2025-05-07 13:54:34 +0800

    init

---
+++ autoDemo/src/main/java/com/example/autoDemo/AutoDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.autoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoDemoApplication.class, args);
+    }
+}
+
